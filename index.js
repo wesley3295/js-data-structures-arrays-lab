b69fb409fb131f1d5a1c9914313598fb841b018a
Wesley <wspencer3295@yahoo.com>
2021-02-11 12:25:18 -0700

    commit tree 98a0dca31eb9fa01a4ee89920eb522158a6e4380

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver (name) {
+    drivers.push(name);
+}
+function destructivelyPrependDriver (name) {
+    drivers.unshift(name);
+}
+function destructivelyRemoveDriver (name) {
+    drivers.pop(name);
+}
+function destructivelyRemoveLastDriver () {
+    drivers.pop();
+}
+function destructivelyRemoveFirstDriver () {
+    drivers.shift();
+}
+function appendDriver(name){
+    return [...drivers, name];
+}
+function prependDriver(name){
+    return [name,...drivers ];
+}
+function removeLastDriver(){
+    return drivers.slice(0, drivers.length - 1);
+}
+function removeFirstDriver(){
+    return drivers.slice(1);
+}
+// Why doesnt 0 apply here?
